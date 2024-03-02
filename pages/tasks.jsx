788d8410f99b5ea2fc0bfee4c01bd7f5a40279ff
Deep Kumar Khamaru <deepkumarkhamaru@gmail.com>
2024-03-02 16:22:29 +0530

    updated tasks display

--- pages/tasks.jsx
+++ pages/tasks.jsx
@@ -2,6 +2,8 @@ import Layout from '../components/layout';
 import { getCookie } from 'cookies-next';
 import Link from 'next/link';
 import clientPromise from "../lib/mongodb";
+import { HiOutlineDuplicate } from "react-icons/hi";
+import { RxCrossCircled } from "react-icons/rx";
 
 export default function ProfilePage( {username, tasks} ) {
 	return (
@@ -9,11 +11,43 @@ export default function ProfilePage( {username, tasks} ) {
 			<Link href="/">Home</Link><br/>
 			<h2>{username}'s Tasks</h2>
 			<p>List of Tasks</p>
-			<>
+			<div className='w-full flex flex-row justify-center'>
 			{JSON.parse(tasks).map((item, index) => 
-				(<div key={item.password_hash}><p>{JSON.stringify(item)}</p><br/></div>)
+				(<div
+						key={`${item}-${index}`}
+						className="w-1/2 search-selection"
+					>
+						<div
+							className="w-auto item outsight"
+						></div>
+						<div
+							className="w-auto h-[100px] border-2 border-solid border-blue-200 items-center pl-3 pr-5 flex flex-row justify-start rounded-md hover:bg-slate-700 cursor-pointer"
+						>
+							<div
+								role="textbox"
+								className="px-1 mx-3 w-full h-[20px] text-white focus:outline-none text-2xl flex flex-row justify-start items-center bg-transparent whitespace-nowrap"
+							>
+									<span
+										className="highlight rounded-md px-2 ml-1"
+									>
+									</span>
+							</div>
+							<div className="flex flex-col h-full justify-evenly">
+								<RxCrossCircled
+									size={35}
+									className="cursor-pointer text-slate-400"
+									onClick={() => closeOption(index)}
+								/>
+								<HiOutlineDuplicate
+									size={35}
+									className="cursor-pointer text-slate-400"
+									onClick={() => duplicateIdea(index)}
+								/>
+							</div>
+						</div>
+					</div>)
 			)}
-			</>
+			</div>
 		</Layout>
 	);
 }
